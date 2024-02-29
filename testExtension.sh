ef78612f5e0762c7159ece5a815311f71e181147
ruben <ruben@zukic.net>
2024-02-29 19:07:06 +0100

    cleanup

--- testExtension.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf latex
-mkdir latex latex/grammars latex/languages latex/languages/latex
-
-cp extension.json latex
-cp grammars/latex.wasm latex/grammars
-cp -r languages/latex/* latex/languages/latex
