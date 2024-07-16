d7fc3e595c0ed786554580ec7663dae35503d74d
lnay <luke@grahamnaylor.net>
2024-07-16 16:08:49 +0100

    Correct queries for language injections (tested)

--- languages/latex/injections.scm
+++ languages/latex/injections.scm
@@ -2,32 +2,32 @@
   (line_comment)
   (block_comment)
   (comment_environment)
-] @injection.content
-  (#set! injection.language "comment"))
+] @content
+  (#set! "language" "comment"))
 
 (pycode_environment
-  code: (source_code) @injection.content
-  (#set! injection.language "python"))
+  code: (source_code) @content
+  (#set! "language" "python"))
 
 (sagesilent_environment
-  code: (source_code) @injection.content
-  (#set! injection.language "python"))
+  code: (source_code) @content
+  (#set! "language" "python"))
 
 (sageblock_environment
-  code: (source_code) @injection.content
-  (#set! injection.language "python"))
+  code: (source_code) @content
+  (#set! "language" "python"))
 
 (minted_environment
   (begin
     language:
       (curly_group_text
-        (text) @injection.language))
-  (source_code) @injection.content)
+        (text) @language))
+  (source_code) @content)
 
 ((generic_environment
   (begin
     name:
       (curly_group_text
-        (text) @_env))) @injection.content
-  (#set! injection.language "c")
+        (text) @_env))) @content
+  (#set! "language" "c")
   (#any-of? @_env "asy" "asydef"))
