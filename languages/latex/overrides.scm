352652d19963ecd5719fa8ad3d4f3641d53d4409
lnay <luke@grahamnaylor.net>
2024-12-08 23:54:15 +0000

    Add overrides to make auto-pairs brackets more specific

---
+++ languages/latex/overrides.scm
@@ -0,0 +1,15 @@
+;; Math
+[
+  (displayed_equation)
+  (inline_formula)
+] @math
+
+(math_environment
+  (_) @math)
+
+;; Comments
+[
+  (line_comment)
+  (block_comment)
+  (comment_environment)
+] @comment
